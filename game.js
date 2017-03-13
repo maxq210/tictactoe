045e81331e5f7dcaeb02ddba47e13ed321b1b0df
Max <maxq210@gmail.com>
2017-03-13 12:09:46 -0700

    v1 working

---
+++ game.js
@@ -0,0 +1,37 @@
+var currPlayer = true;
+var o = [];
+var x = [];
+var solutions = [
+	[1,5,9], [3,5,7], [1,2,3], [4,5,6], [7,8,9], [1,4,7], [2,5,8], [3,6,9]
+];
+
+function clickHandler(id) {
+	//console.log(id);
+	//console.log("inner: " + document.getElementById(id).innerHTML)
+	if(document.getElementById(id).innerHTML === '' && currPlayer){
+		document.getElementById(id).innerHTML = "X";
+		x.push(parseInt(id));
+		console.log(x);
+		checkSol(x);
+		currPlayer = !currPlayer;
+	}
+	else if (document.getElementById(id).innerHTML === ''){
+		document.getElementById(id).innerHTML = "O";
+		o.push(id);
+		currPlayer = !currPlayer;
+	}
+}
+
+function checkSol(player) {
+	for(var i = 0; i < solutions.length; i++) {
+		for(var j = 0; j < 3; j++) {
+			if(player.indexOf(solutions[i][j]) === -1){
+				break;
+			}
+			if(j === 2){
+				console.log("Winner!")
+				return;
+			}
+		}
+	}
+}
